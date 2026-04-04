25e46eaa74394d5d2ba745198cedf73aacbdc884
Lordpun <=>
2026-04-04 16:09:23 -0400

    Connected commands

--- background.py
+++ background.py
@@ -37,17 +37,17 @@ def setTerminalColor():
     path = result.strip().replace("Image=file://", "")
     color = getColor(path)
 
-    # setCurrentBackgrounds(color)
-    # editKittyConfig(color)
-  except Exception:
-    sys.exit("Failed to find your wallpaper\'s path")
+    setCurrentBackgrounds(color)
+    editKittyConfig(color)
+  except Exception as e:
+    sys.exit(f"Failed to find your wallpaper\'s path: {e}")
 
 def editKittyConfig(color):
   conf_path = os.path.expanduser("~/.config/kitty/kitty.conf")
     
   if os.path.exists(conf_path):
-      with open(conf_path, 'r') as f:
-        lines = f.readlines()
+    with open(conf_path, 'r') as f:
+      lines = f.readlines()
   else:
     lines = []
 
@@ -55,7 +55,7 @@ def editKittyConfig(color):
   new_lines.append(f"background {color}\n")
 
   with open(conf_path, 'w') as f:
-      f.writelines(new_lines)
+    f.writelines(new_lines)
 
 def setCurrentBackgrounds(color):
   try:
