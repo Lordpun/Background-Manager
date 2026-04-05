4d23721e419b6afaeb393cbabc35b7aa893fec9a
Lordpun <=>
2026-04-04 21:36:48 -0400

    Made a function to get the wallpaper path

--- background.py
+++ background.py
@@ -1,12 +1,23 @@
+from pathlib import Path
 import subprocess
 import sys
 import os
 import extcolors
 
+wallpaperPath = Path.home() / ".config" / "plasma-org.kde.plasma.desktop-appletsrc"
+kittyConfig = Path.home() / ".config" / "kitty" / "kitty.conf"
+
 def getColor(filePath):
   colors, pixel_count = extcolors.extract_from_path("image.jpg", tolerance=20)
   main_color = colors[0][0]
   return '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2])
+
+def getWallPaper():
+  with open(wallpaperPath, "w") as config:
+    lines = config.readlines()
+    for line.strip() in lines:
+      if "Image=file://" in line:
+        return line.replace("Image=file://", "", 1)
 
 def setWallpaper(filePath):
   pass
@@ -18,4 +29,5 @@ def editKittyConfig(color):
   pass
 
 def setCurrentBackgrounds(color):
+  pass
   pass
