cc59ae897392e72ac277ea57c5059269ade714f6
Lordpun <=>
2026-04-05 00:35:13 -0400

    Forgot getWallpaper was still used

--- background.py
+++ background.py
@@ -4,7 +4,15 @@ import sys
 import os
 import extcolors
 
+wallpaperPath = Path.home() / ".config" / "plasma-org.kde.plasma.desktop-appletsrc"
 kittyConfig = Path.home() / ".config" / "kitty" / "kitty.conf"
+
+def getWallPaper():
+  with open(wallpaperPath, "r") as config:
+    lines = config.readlines()
+    for line.strip() in lines:
+      if "Image=file://" in line:
+        return line.replace("Image=file://", "", 1)
 
 def getColor():
   path = getWallpaper()
