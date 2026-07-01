2569ea351aa33a623861ac0dd3d7c19aa48cc470
Lordpun <thedirtcart@gmail.com>
2026-07-01 16:54:51 -0400

    Added support for addon scripts

---
+++ addons.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import json
+import subprocess
+
+addonsList = Path.home() / ".config" / "BgManager" / "addons.json"
+
+def getAddonsFile():
+  if not addonsList.exists():
+    with open(addonsList, "w") as file:
+    json.dump([], file)
+
+  with open(addonsList, "r") as file:
+    return json.load(file)
+
+def getAddonScripts():
+  addons = getAddonsFile()
+
+  for addon in addons:
+    subprocess.run(addon, shell=True)
